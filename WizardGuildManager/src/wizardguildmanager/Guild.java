48379e3405bf7830021bde385af0c814d16ae2a9
Yanis BERTHEBAUD <yanis.berthebaud@isen.yncrea.fr>
2021-10-18 16:02:29 +0200

    complétion de guild

--- WizardGuildManager/src/wizardguildmanager/Guild.java
+++ WizardGuildManager/src/wizardguildmanager/Guild.java
@@ -16,5 +16,75 @@ public class Guild {
     private Boolean typeOfGuild;
     private ArrayList <Member> members;
     private Integer numberOfMembers;
-           
+    private ArrayList <Mission> availableMissions;
+
+    public Guild(String name, String slogan, Boolean typeOfGuild, ArrayList<Member> members, Integer numberOfMembers, ArrayList<Mission> availableMissions) {
+        this.name = name;
+        this.slogan = slogan;
+        this.typeOfGuild = typeOfGuild;
+        this.members = members;
+        this.numberOfMembers = numberOfMembers;
+        this.availableMissions = availableMissions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public Boolean getTypeOfGuild() {
+        return typeOfGuild;
+    }
+
+    public ArrayList<Member> getMembers() {
+        return members;
+    }
+
+    public Integer getNumberOfMembers() {
+        return numberOfMembers;
+    }
+
+    public ArrayList<Mission> getAvailableMissions() {
+        return availableMissions;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+
+    public void setTypeOfGuild(Boolean typeOfGuild) {
+        this.typeOfGuild = typeOfGuild;
+    }
+
+    public void setNumberOfMembers(Integer numberOfMembers) {
+        this.numberOfMembers = numberOfMembers;
+    }
+
+    public void setMembers(ArrayList<Member> members) {
+        this.members = members;
+    }
+
+    public void setAvailableMissions(ArrayList<Mission> availableMissions) {
+        this.availableMissions = availableMissions;
+    }
+    
+    public void addMember(Member member) {
+        this.members.add(member);
+    }
+    public void removeMember(Member member) {
+        this.members.remove(member);
+    }
+    public void addMission(Mission mission) {
+        this.availableMissions.add(mission);
+    }
+    public void removeMission(Mission mission) {
+        this.availableMissions.remove(mission);
+    }
 }
